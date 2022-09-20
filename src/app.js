835927097fff50786593b8c9efa920f58b6a7591
Alejandra <otro@gmail.com>
2022-09-20 19:11:55 -0300

    vistas

--- src/app.js
+++ src/app.js
@@ -1,12 +1,14 @@
 const express = require('express') //requerir express
 const app = express() //instanciar una app de express
+const path = require('path')
 const PORT = 3000
 
-const author = require('../models').authors;
-const book = require('../models').books;
-app.use(express.json())
+app.use(express.static('public'))
+app.use('/prefijo', express.static('publica'))
+
 app.get('/', (req, res) => {
     res.send('Hola chicos!')
 })
+
 
 app.listen(PORT, () => console.log('escuchando en el puerto', PORT))
---
+++ src/routes/products.js
@@ -0,0 +1,10 @@
+const express = require('express') //requerir express
+const router = express.Router();
+
+//creo rutas
+router.get('/cargar-producto', () => {
+    //retornar vista
+    
+})
+
+module.exports = router

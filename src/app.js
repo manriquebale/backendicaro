46598250d7589e0de6a9dbd5134a11b31d2a383c
Alejandra <otro@gmail.com>
2022-09-20 19:40:21 -0300

    for

--- src/app.js
+++ src/app.js
@@ -7,15 +7,33 @@ app.use('/prefijo', express.static('publica'))
 
 //Registrar ejs como motor de plantillas
 app.set('view engine', 'ejs')
-
 app.set('views', path.join(__dirname, 'views'))
 
-app.get('/', (req, res) => {
-    res.send('Hola chicos!')
-})
 
-app.get('/index', (req, res) => {
-    res.render('index')
+
+
+
+
+app.get('/', (req, res) => {
+
+    const usuario =  [
+        {
+            nombre: 'ale',
+            apellido: 'manrique'
+        },
+        {
+            nombre: 'agus',
+            apellido: 'mperez'
+        },
+        {
+            nombre: 'marioa',
+            apellido: 'lopez'
+        }
+
+
+    ] 
+
+    res.render('index', { usuario})
     // res.render('../src/views/index.ejs')
 
 })
